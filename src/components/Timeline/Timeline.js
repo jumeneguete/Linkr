94c92cc8b47dee2ea1d6a43f2a8962a1f0117688
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-05-25 18:52:15 -0300

    Renderizando posts via servidor

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { PageTitle, ContainerPostsAndTrendings, Trendings, CreatePost, ContainerPosts } from "./Styles";
 import GlobalStyle from '../../styles/GlobalStyle';
 import Post from './Post';
 
 export default function Timeline() {
+    const token = '8181382a-f871-4195-ade8-982e9eb999fa';
+    const [postsList, setPostsList] = useState(null);
 
+    useEffect(() => {
+        const config ={ headers: { Authorization: `Bearer ${token}` }}
+        const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts', config);
+        request.then(response => {
+            setPostsList(response.data.posts)
+        });
+        request.catch(erro => alert("Ocorreu um erro ao carregar os posts"))
+    }, [])
 
     return(
         <>
@@ -12,8 +25,7 @@ export default function Timeline() {
             <ContainerPostsAndTrendings>
                 <ContainerPosts>
                     <CreatePost>Em breve</CreatePost>
-                    <Post/>
-                    <Post/>
+                    {postsList && postsList.map(p => <Post key ={p.id} postDetails={p}/>)}
                 </ContainerPosts>
                 <Trendings>Em breve</Trendings>
             </ContainerPostsAndTrendings>
