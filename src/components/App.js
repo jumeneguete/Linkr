4822f76ad620cf56b268fe945689931895c6b625
Juliana Meneguete <jumeneguete@gmail.com>
2021-05-25 17:34:39 -0300

    GlobalStyle - corrigido

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import GlobalStyle from '../styles/GlobalStyle';
+import GlobalStyle from "../styles/GlobalStyle";
 import Login from './Login_SignUp/Login';
 import SignUp from './Login_SignUp/SignUp';
 import Timeline from './Timeline/Timeline';
