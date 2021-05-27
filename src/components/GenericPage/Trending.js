35d710de0e5c4e52783d7e59c42c7e2763e7a54c
Juliana Meneguete <jumeneguete@gmail.com>
2021-05-27 14:44:26 -0300

    Estilos de Trending em 'Styles.js'

--- src/components/GenericPage/Trending.js
+++ src/components/GenericPage/Trending.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { TrendingStyle, Title, Separator, TrendingList } from "./Styles.js";
 import UserContext from "../../contexts/UserContext";
 
 export default function Trending (){
@@ -36,43 +36,3 @@ export default function Trending (){
     );
 }
 
-const TrendingStyle = styled.div`
-    width: 301px;
-    height: 406px;
-    background: #171717;
-    border-radius: 16px;
-    color: #fff;
-    display: flex;
-    flex-direction: column;
-    position: sticky;
-    right: auto;
-    top: 200px;
-
-    @media (max-width: 614px) {
-        display: none;
-    }
-`;
-
-const Title = styled.div`
-    font-family: "Oswald", sans-serif;
-    font-size: 27px;
-    font-weight: 700;
-    padding: 15px;
-`;
-
-const Separator = styled.div`
-    width: 100%;
-    height: 1px;
-    background-color: #484848;
-`;
-
-const TrendingList = styled.ul`
-    padding: 15px;
-    
-    li {
-        font-size: 18px;
-        font-weight: 700;
-        letter-spacing: 1px;
-        margin-top: 12px;
-    }
-`;
