4c13db346f9e7ed023d99794edb06d65493f83c0
Juliana Meneguete <jumeneguete@gmail.com>
2021-05-28 09:24:40 -0300

    Arquivos deletados

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import UserContext from '../contexts/UserContext';
 
 import GlobalStyle from "../styles/GlobalStyle";
