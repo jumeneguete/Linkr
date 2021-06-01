3faa1e4b45bc87e65fb28931a3cbb0054a5ef1e0
Juliana Meneguete <jumeneguete@gmail.com>
2021-06-01 15:27:10 -0300

    estilos separados

--- src/components/SinglePost/PostComments.js
+++ src/components/SinglePost/PostComments.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import {CommentBox, Comments, CommentInfo, Divider, AddCommentForm, InputComment, ButtonComment} from "./Styles"
 import { BsDot } from 'react-icons/bs';
 import { IoPaperPlaneOutline } from 'react-icons/io5';
 import { useContext, useEffect, useState } from "react";
@@ -57,7 +57,7 @@ export default function PostComments({ setComments, openComments, comments, Post
                         <AddCommentForm onSubmit={SendComment}>
                             <Link to="#"><img src={userProfile.user.avatar} alt={userProfile.user.username} /></Link>
                             <div>
-                                <Input placeholder="write a comment..." onChange={(e)=> setNewComment(e.target.value)} value={newComment} />
+                                <InputComment placeholder="write a comment..." onChange={(e)=> setNewComment(e.target.value)} value={newComment} />
                                 <ButtonComment type="submit"><IoPaperPlaneOutline color={"#fff"} /></ButtonComment>
                             </div>
                         </AddCommentForm>
@@ -65,120 +65,3 @@ export default function PostComments({ setComments, openComments, comments, Post
     );
 }
 
-const CommentBox = styled.div`
-    width: 100%;
-    background-color: #1e1e1e;
-    margin: -25px 0 16px 0;
-    border-radius: 0 0 16px 16px;
-    display: ${props => props.openComments ? "block" : "none"};
-    z-index: 0;
-
-    @media (max-width: 614px) {
-    border-radius: 0;
-}
-    
-`;
-
-const Comments = styled.div`
-    padding: 10px 20px; 
-    display: flex;
-    align-items: flex-start;
-
-    img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        margin-left: 10px;
-        object-fit: cover;
-        margin-right: 13px;
-    }
-
-
-`;
-
-const CommentInfo = styled.div`
-    width: 85%;
-        font-size: 14px;
- 
-    div {
-        display: flex;
-        align-items: center;
-        margin-bottom: 7px;
-
-        p {
-            color: #fff;
-            font-weight: 700;
-        }
-
-        span {
-            color: #565656;
-        }
-    }
-
-    & > span {
-        word-break: break-all;
-        color: #acacac;
-    }
-    
-`;
-
-const Divider = styled.div`
-    height: 1px;
-    background-color: #353535;
-    margin: 0 20px;
-    
-`;
-
-const AddCommentForm = styled.form`
-    padding: 15px 20px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-   
-    img {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        margin-left: 10px;
-        object-fit: cover;
-        margin-right: 13px;
-    }
-
-    & > div {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-    }
-`;
-
-const Input = styled.input`
-    width: 100%;
-    height: 40px;
-    padding-left: 10px;
-    color: #fff;
-    border: none;
-    border-radius: 8px 0 0 8px;
-    background-color: #252525;
-
-    &:focus{
-        box-shadow: 0 0 0 0;
-        outline: 0;
-    }
-
-    &::placeholder {
-        color: #575757;
-        font-weight: 700;
-        font-style: italic;
-    }
-`;
-
-const ButtonComment = styled.button`
-    height: 40px;
-    border: none;
-    padding: 0 15px;
-    border-radius: 0 8px 8px 0;
-    background-color: #252525;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
--- src/components/SinglePost/Styles.js
+++ src/components/SinglePost/Styles.js
@@ -194,4 +194,122 @@ const CommentsContainer = styled.div`
 
 `;
 
-export { SinglePost, Profile, PostContent, CreatorName, Description, LinkContainer, LinkInfo, LinkImg, Hashtag, LikesContainer, StyledReactTooltip, CommentsContainer };
+const CommentBox = styled.div`
+    width: 100%;
+    background-color: #1e1e1e;
+    margin: -25px 0 16px 0;
+    border-radius: 0 0 16px 16px;
+    display: ${props => props.openComments ? "block" : "none"};
+    z-index: 0;
+
+    @media (max-width: 614px) {
+    border-radius: 0;
+}
+    
+`;
+
+const Comments = styled.div`
+    padding: 10px 20px; 
+    display: flex;
+    align-items: flex-start;
+
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-left: 10px;
+        object-fit: cover;
+        margin-right: 13px;
+    }
+
+
+`;
+
+const CommentInfo = styled.div`
+    width: 85%;
+        font-size: 14px;
+ 
+    div {
+        display: flex;
+        align-items: center;
+        margin-bottom: 7px;
+
+        p {
+            color: #fff;
+            font-weight: 700;
+        }
+
+        span {
+            color: #565656;
+        }
+    }
+
+    & > span {
+        word-break: break-all;
+        color: #acacac;
+    }
+    
+`;
+
+const Divider = styled.div`
+    height: 1px;
+    background-color: #353535;
+    margin: 0 20px;
+    
+`;
+
+const AddCommentForm = styled.form`
+    padding: 15px 20px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+   
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-left: 10px;
+        object-fit: cover;
+        margin-right: 13px;
+    }
+
+    & > div {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const InputComment = styled.input`
+    width: 100%;
+    height: 40px;
+    padding-left: 10px;
+    color: #fff;
+    border: none;
+    border-radius: 8px 0 0 8px;
+    background-color: #252525;
+
+    &:focus{
+        box-shadow: 0 0 0 0;
+        outline: 0;
+    }
+
+    &::placeholder {
+        color: #575757;
+        font-weight: 700;
+        font-style: italic;
+    }
+`;
+
+const ButtonComment = styled.button`
+    height: 40px;
+    border: none;
+    padding: 0 15px;
+    border-radius: 0 8px 8px 0;
+    background-color: #252525;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export { SinglePost, Profile, PostContent, CreatorName, Description, LinkContainer, LinkInfo, LinkImg, Hashtag, LikesContainer, StyledReactTooltip, CommentsContainer, CommentBox, Comments, CommentInfo, Divider, AddCommentForm, InputComment, ButtonComment };
